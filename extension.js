92f97ded7c20b600c596a6a0ca25979cd61534dd
maniacx <macs18max@gmail.com>
2024-01-22 22:15:21 -0800

    Minor updates

--- extension.js
+++ extension.js
@@ -1,13 +1,13 @@
 'use strict';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as BluetoothBatteryMeter from './lib/blutoothToggle.js';
+import * as BluetoothToggle from './lib/bluetoothToggle.js';
 
 export default class BluetoothBatteryMeterExtension extends Extension {
     // Bluetooth quicks settings menu are accessible when the session is locked.
     // Therefore unlock-dialog session mode is used.
     enable() {
         this._settings = this.getSettings();
-        this._btBatteryMeter = new BluetoothBatteryMeter.BluetoothToggle(this, this._settings);
+        this._btBatteryMeter = new BluetoothToggle.BluetoothBatteryMeter(this, this._settings);
     }
 
     disable() {
